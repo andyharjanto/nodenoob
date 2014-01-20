1756a163b801f1916aaf03a08ad247e3ac256dd6
Andy Harjanto <andy@tellwise.com>
2014-01-20 13:24:50 -0800

    Initial commit

---
+++ controller/account.js
@@ -0,0 +1,35 @@
+﻿
+
+exports.addToGuestBook = function(req, res) {
+    return new addToGuestBook(req,res);
+    }
+
+exports.getUsers = function(res ) { 
+    return new getUsers(res);
+    }
+
+var Guests = [];
+
+/// Simply just user to the guest book
+function addToGuestBook(req, res) {
+      var userName = req.body.userName;
+      if (!userName) {
+          res.send("Missing parameter: userName");
+          return;
+         };
+
+     var user = {};
+     user.userName = userName;
+     user.visited = Date.now();
+     Guests.unshift(user); // add user at the beginning of the array
+     res.send(user);
+    };
+
+
+function getUsers(res) {
+    return res.send(Guests);
+    }
+
+
+
+
---
+++ server.js
@@ -0,0 +1,56 @@
+/// Andy Harjanto
+/// January 19,2014
+/// We're going to implement a simple REST service with JSON as the data exchange
+/// With MVC pattern
+/// You can use CURL or other HTTP utility as a client. 
+/// We'll do the simplest form first. 
+
+// use express to route request and http communication
+/// you can do many different ways of routing with express
+var express = require('express');
+
+var account = require('./controller/account.js'); // this is our own module
+
+// setup app to parse Json and encode URL
+app = express();
+app.use(express.json());
+app.use(express.urlencoded());
+
+var port = process.env.port || 1337;
+
+// Root API
+app.get('/', function(req, res){
+  res.send('Put some API usage here if you like');
+});
+
+/// Simple Get
+/// Example what it returns
+/*
+[
+  {
+    "userName": "mgray",
+    "visited": 1390252064656
+  },
+  {
+    "userName": "johnsmith",
+    "visited": 1390252040618
+  }
+]
+*/
+app.get('/account', function(req, res) {
+    account.getUsers(res);
+    });
+
+// Simple Post 
+/// Example of the body POST (note, the client must set Content-Type: application/json)
+/*
+   { 
+    "userName":"mgray"
+   }
+*/
+
+app.post('/account', function(req, res) {
+    account.addToGuestBook(req, res);
+    });
+
+app.listen(port);
